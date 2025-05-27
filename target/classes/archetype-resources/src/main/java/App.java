353391764249dfe43efc2a58a8ea3d05728bd7d5
VSNVenkataManikanta <venkata.manikanta.vemulamada@hexagon.com>
2025-05-27 08:09:37 +0530

    Add files via upload
    1st upload

---
+++ target/classes/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $RESTAssuredPOC.RESTAssuredPOC;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
